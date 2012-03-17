dc1e184fecea153d1355575c6d6f2689cc05feb7
Takafumi Arakaki <aka.tkf@gmail.com>
2012-03-17 20:41:40 +0100

    document monky-cmdserver-read

--- monky.el
+++ monky.el
@@ -354,6 +354,7 @@ refreshes buffers."
       (vconcat str))))
 
 (defun monky-cmdserver-read ()
+  "Read one channel and return cons (CHANNEL . RAW-DATA)."
   (let* ((data (bindat-unpack '((channel byte) (len u32))
                               (monky-cmdserver-read-data 5)))
          (channel (bindat-get-field data 'channel))
